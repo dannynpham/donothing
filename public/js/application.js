421047130621102a6ffa37899891d4ff0d44457c
Danny Pham <email": null>
2016-06-17 08:32:28 -0700

    didn't do anything

--- public/js/application.js
+++ public/js/application.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var sizes = [40,50,60,70,90,110,125,140,160,180]
     var randomX = Math.floor(Math.random()*$(document).height());
     var randomY = Math.floor(Math.random()*$(document).width());
-    var randomTilt = (Math.floor(Math.random()*300)+1);
+    var randomTilt = (Math.floor(Math.random()*280)+1);
     var randomSz = sizes[Math.floor(Math.random()*sizes.length)];
     return [randomX,randomY,randomTilt,randomSz];
   }
@@ -24,30 +24,26 @@ $(document).ready(function() {
     return img;
   }
 
-  $('#ricky').hide();
-  $('#end').hide();
-
-  document.addEventListener("mousemove", function(event) {
+  $(document).mousemove(function(event){
+    $("#ricka").css({left:event.pageX-60, top:event.pageY-60});
     $("#video").get(0).currentTime = 0;
     ricky();
-  });
-
-  $(document).mousemove(function(event){
-    $("#ricka").css({left:event.pageX-70, top:event.pageY-70});
   });
 
   var callback = function () {
     $('.rik').remove();
     $('#title').remove();
     $('#video').remove();
+    $('button').show();
     $('#end').show();
     $('#ricky').show();
   }
-  $("#video").on("ended", callback);
-});
 
-document.addEventListener("keypress", function(event) {
-  if (event.keyCode === 48) {
+  $("#video").on("ended", callback);
+  });
+
+  document.addEventListener("keypress", function(event) {
+    if (event.keyCode === 48) {
     $("#video").get(0).currentTime += 20;
     $('.rik').remove();
   }
